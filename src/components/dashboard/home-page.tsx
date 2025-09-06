7d2346a49ce26012bc826593fb4df569ecb1b4f9
Bhavishya Jain <bhavishyajain011@gmail.com>
2025-09-06 02:17:20 +0000

    like first top potion covers market pulse and then the bottom part give

--- src/components/dashboard/home-page.tsx
+++ src/components/dashboard/home-page.tsx
@@ -25,13 +25,9 @@ export function HomePage({dictionary}: {dictionary: Dictionary}) {
 
   return (
     <Dashboard dictionary={dictionary}>
-      <div className="grid grid-cols-1 gap-6 xl:grid-cols-2 xl:gap-8">
-        <div className="flex flex-col gap-6 xl:gap-8">
-          <MarketPulse dictionary={dictionary} />
-        </div>
-        <div className="flex flex-col gap-6 xl:gap-8">
-          <RecentCreations dictionary={dictionary} />
-        </div>
+      <div className="flex flex-col gap-6 xl:gap-8">
+        <MarketPulse dictionary={dictionary} />
+        <RecentCreations dictionary={dictionary} />
       </div>
     </Dashboard>
   );
